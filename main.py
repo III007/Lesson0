4fad854f94b29f6d675cd6569cb4ba17de0fa7b4
III007 <curren212@gmail.com>
2024-08-01 01:15:08 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+# 1st program
+print(9 ** 0.5 * 5)
+# 2nd program
+print(9.99 > 9.98 and 1000 != 1000.1)
+# 3rd program
+print(1234 // 10 % 100 + 5678 // 10 % 100)
+# 4nd program
+print(int(13.42) == int(42.13 % 10 * 100 % 100) or int(13.42 % 10 * 100 % 100) == int(42.13))
